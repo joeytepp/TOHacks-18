59ef60302db24c113e54ab261845d987532a15ba
Noah Ifergan <noahifergan@Noahs-MacBook-Pro.local>
2018-05-26 17:10:57 -0400

    delete files

--- client/views/navbar.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("navMenu").innerHTML =
- '<ul>'+
-  '<li><a href="home.html">Home</a></li>'+
-  '<li><a href="about.html">About</a></li>'+
-  '<li><a href="login.html">Log In</a></li>'+
-  '<li><a href="signup.html">Sign Up</a></li>'+
- '</ul>';
